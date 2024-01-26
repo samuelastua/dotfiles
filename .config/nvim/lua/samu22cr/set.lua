10fa92e3d1e3f05716f5bf6f9b302a80a67e53ca
samu22cr <samuelastuaflores@gmail.com>
2024-01-26 15:11:34 -0600

    adding tmux conf (think I messed up something related to git, not sure)

--- .config/nvim/lua/samu22cr/set.lua
+++ .config/nvim/lua/samu22cr/set.lua
@@ -3,10 +3,15 @@ vim.g.maplocalleader = ' '
 
 vim.wo.number = true
 vim.wo.relativenumber = true
+<<<<<<< HEAD
+=======
+
+>>>>>>> 087c049 (adding tmux conf)
 vim.g.netrw_banner = 0
 vim.o.mouse = 'a'
 vim.o.breakindent = true
 vim.o.undofile = true
+<<<<<<< HEAD
 vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.updatetime = 250
@@ -19,6 +24,24 @@ vim.o.termguicolors = true
 
 vim.o.hlsearch = false
 vim.o.incsearch = true
+=======
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+vim.o.completeopt = 'menuone,noselect'
+
+
+-- searching
+vim.o.hlsearch = false
+vim.o.incsearch = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- UI
+vim.o.termguicolors = true
+
+-- miscellaneous
+vim.o.splitright = true
+>>>>>>> 087c049 (adding tmux conf)
 
 vim.o.colorcolumn = "90"
 vim.o.signcolumn = "auto"
@@ -26,7 +49,15 @@ vim.o.signcolumn = "auto"
 vim.opt.isfname:append("@-@")
 vim.opt.scrolloff = 30
 
+<<<<<<< HEAD
+=======
+-- tabs
+>>>>>>> 087c049 (adding tmux conf)
 vim.o.shiftwidth = 4
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.expandtab = true
+<<<<<<< HEAD
+=======
+
+>>>>>>> 087c049 (adding tmux conf)
